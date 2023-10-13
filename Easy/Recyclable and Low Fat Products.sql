ca9efec0a6e49abd0605e2a6f6dc4855706d6689
vignesh1166 <124414781+vignesh1166@users.noreply.github.com>
2023-10-13 19:21:39 +0530

    Recyclable and Low Fat Products

---
+++ Easy/Recyclable and Low Fat Products.sql
@@ -0,0 +1,32 @@
+'''
+  Write a solution to find the ids of products that are both low fat and recyclable.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Products table:
++-------------+----------+------------+
+| product_id  | low_fats | recyclable |
++-------------+----------+------------+
+| 0           | Y        | N          |
+| 1           | Y        | Y          |
+| 2           | N        | Y          |
+| 3           | Y        | Y          |
+| 4           | N        | N          |
++-------------+----------+------------+
+Output: 
++-------------+
+| product_id  |
++-------------+
+| 1           |
+| 3           |
++-------------+ 
+
+solution '''
+select product_id from Products where low_fats = 'Y' and recyclable = 'Y';

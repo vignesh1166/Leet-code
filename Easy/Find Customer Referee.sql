0a23540cdf0d3976a38f7c287880f3d9186419ff
Vignesh M <124414781+vignesh1166@users.noreply.github.com>
2023-10-14 09:18:03 +0530

    Create Find Customer Referee.sql

---
+++ Easy/Find Customer Referee.sql
@@ -0,0 +1,35 @@
+'''
+Find the names of the customer that are not referred by the customer with id = 2.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Customer table:
++----+------+------------+
+| id | name | referee_id |
++----+------+------------+
+| 1  | Will | null       |
+| 2  | Jane | null       |
+| 3  | Alex | 2          |
+| 4  | Bill | null       |
+| 5  | Zack | 1          |
+| 6  | Mark | 2          |
++----+------+------------+
+Output: 
++------+
+| name |
++------+
+| Will |
+| Jane |
+| Bill |
+| Zack |
++------+ 
+
+Solution '''
+select name from Customer where not referee_id = 2 or referee_id IS NULL;

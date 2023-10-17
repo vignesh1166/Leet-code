c45cbc91bde0ecfad1a54807f9446042db7acbd3
Vignesh M <124414781+vignesh1166@users.noreply.github.com>
2023-10-17 20:09:41 +0530

    Invalid Tweets.sql

---
+++ Easy/Invalid Tweets.sql
@@ -0,0 +1,28 @@
+'''
+Write a solution to find the IDs of the invalid tweets. The tweet is invalid if the number of characters used in the content of the tweet is strictly greater than 15.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Tweets table:
++----------+----------------------------------+
+| tweet_id | content                          |
++----------+----------------------------------+
+| 1        | Vote for Biden                   |
+| 2        | Let us make America great again! |
++----------+----------------------------------+
+Output: 
++----------+
+| tweet_id |
++----------+
+| 2        |
++----------+
+
+Solution'''
+select tweet_id from Tweets where Char_length(content)>15;

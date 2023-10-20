4dfea047ba335e1be44c566c8fb16beb2183a452
Vignesh M <124414781+vignesh1166@users.noreply.github.com>
2023-10-20 19:41:10 +0530

    Customer Who Visited but Did Not Make Any Transactions.sql

---
+++ Joins/Customer Who Visited but Did Not Make Any Transactions.sql
@@ -0,0 +1,51 @@
+'''
+Write a solution to find the IDs of the users who visited without making any transactions and the number of times they made these types of visits.
+
+Return the result table sorted in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Visits
++----------+-------------+
+| visit_id | customer_id |
++----------+-------------+
+| 1        | 23          |
+| 2        | 9           |
+| 4        | 30          |
+| 5        | 54          |
+| 6        | 96          |
+| 7        | 54          |
+| 8        | 54          |
++----------+-------------+
+Transactions
++----------------+----------+--------+
+| transaction_id | visit_id | amount |
++----------------+----------+--------+
+| 2              | 5        | 310    |
+| 3              | 5        | 300    |
+| 9              | 5        | 200    |
+| 12             | 1        | 910    |
+| 13             | 2        | 970    |
++----------------+----------+--------+
+Output: 
++-------------+----------------+
+| customer_id | count_no_trans |
++-------------+----------------+
+| 54          | 2              |
+| 30          | 1              |
+| 96          | 1              |
++-------------+----------------+
+
+Solution'''
+
+select Visits.customer_id, count(Visits.visit_id) as count_no_trans
+from Visits
+Left Join Transactions
+ON Visits.visit_id = Transactions.visit_id
+where Transactions.transaction_id IS NULL 
+Group by Visits.customer_id;

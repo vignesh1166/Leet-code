15d26386de3c120f6745ca7cbb3027e452b91b9b
Vignesh M <124414781+vignesh1166@users.noreply.github.com>
2023-10-28 21:33:24 +0530

    Managers with at Least 5 Direct Reports.sql

---
+++ Joins/Managers with at Least 5 Direct Reports.sql
@@ -0,0 +1,37 @@
+'''
+Write a solution to find managers with at least five direct reports.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee table:
++-----+-------+------------+-----------+
+| id  | name  | department | managerId |
++-----+-------+------------+-----------+
+| 101 | John  | A          | None      |
+| 102 | Dan   | A          | 101       |
+| 103 | James | A          | 101       |
+| 104 | Amy   | A          | 101       |
+| 105 | Anne  | A          | 101       |
+| 106 | Ron   | B          | 101       |
++-----+-------+------------+-----------+
+Output: 
++------+
+| name |
++------+
+| John |
++------+
+Solution'''
+  
+select e.name 
+from Employee e
+inner join Employee m
+on e.id = m.managerId
+group by m.managerID 
+having count(*)>=5;

05490737aba1a7d64a3a96e77a0b86c534117a5b
Vignesh M <124414781+vignesh1166@users.noreply.github.com>
2023-11-03 14:28:08 +0530

    Project Employees I.sql

---
+++ Basic Aggregate Functions/Project Employees I.sql
@@ -0,0 +1,46 @@
+'''
+Write an SQL query that reports the average experience years of all the employees for each project, rounded to 2 digits.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Project table:
++-------------+-------------+
+| project_id  | employee_id |
++-------------+-------------+
+| 1           | 1           |
+| 1           | 2           |
+| 1           | 3           |
+| 2           | 1           |
+| 2           | 4           |
++-------------+-------------+
+Employee table:
++-------------+--------+------------------+
+| employee_id | name   | experience_years |
++-------------+--------+------------------+
+| 1           | Khaled | 3                |
+| 2           | Ali    | 2                |
+| 3           | John   | 1                |
+| 4           | Doe    | 2                |
++-------------+--------+------------------+
+Output: 
++-------------+---------------+
+| project_id  | average_years |
++-------------+---------------+
+| 1           | 2.00          |
+| 2           | 2.50          |
++-------------+---------------+
+
+Solution'''
+select p.project_id, round(avg(e.experience_years),2) as average_years 
+from Project p 
+left join Employee e 
+on p.employee_id = e.employee_id
+group by p.project_id;
+

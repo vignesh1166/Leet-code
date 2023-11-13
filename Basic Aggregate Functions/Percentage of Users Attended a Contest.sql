1a7329d47bbaba869e9fc75b51606e37d7ee2474
Vignesh M <124414781+vignesh1166@users.noreply.github.com>
2023-11-13 17:52:45 +0530

    Percentage of Users Attended a Contest.sql

---
+++ Basic Aggregate Functions/Percentage of Users Attended a Contest.sql
@@ -0,0 +1,52 @@
+'''
+Write a solution to find the percentage of the users registered in each contest rounded to two decimals.
+
+Return the result table ordered by percentage in descending order. In case of a tie, order it by contest_id in ascending order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Users table:
++---------+-----------+
+| user_id | user_name |
++---------+-----------+
+| 6       | Alice     |
+| 2       | Bob       |
+| 7       | Alex      |
++---------+-----------+
+Register table:
++------------+---------+
+| contest_id | user_id |
++------------+---------+
+| 215        | 6       |
+| 209        | 2       |
+| 208        | 2       |
+| 210        | 6       |
+| 208        | 6       |
+| 209        | 7       |
+| 209        | 6       |
+| 215        | 7       |
+| 208        | 7       |
+| 210        | 2       |
+| 207        | 2       |
+| 210        | 7       |
++------------+---------+
+Output: 
++------------+------------+
+| contest_id | percentage |
++------------+------------+
+| 208        | 100.0      |
+| 209        | 100.0      |
+| 210        | 100.0      |
+| 215        | 66.67      |
+| 207        | 33.33      |
++------------+------------+
+Solution'''
+select contest_id, round((count(user_id)/(select count(user_id)from users)) * 100,2) as percentage
+from register
+group by contest_id
+order by percentage desc, contest_id;
